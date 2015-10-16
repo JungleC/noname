de0b0f4b1a81ba5e1017ecc3722bedfba0ced6a1
Shapoval Ivan <shapovalvanya@gmail.com>
2015-10-16 21:41:14 +0300

    V0.1.2

--- app/src/main/java/ua/com/alc/alcotaste/MainActivity.java
+++ app/src/main/java/ua/com/alc/alcotaste/MainActivity.java
@@ -5,36 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
-import ua.com.alc.alcotaste.Fragments.FragmentMain;
-import ua.com.alc.alcotaste.Fragments.FragmentSearch;
-import android.app.Fragment;
-import android.app.FragmentManager;
-
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 
-public class MainActivity extends AppCompatActivity
-        implements FragmentMain.OnFloatingActionButtonListener {
+public class MainActivity extends AppCompatActivity {
 
-    private static  int sOnBackPressCounter = 0;
 
     Drawer result = null;
-    FragmentManager fragmentManager = null;
 
-    public void onFloatingActionButtonClick() {
-        Fragment fragment = new FragmentSearch();
-        fragmentManager.beginTransaction()
-                .setCustomAnimations(R.animator.fade_in, R.animator.fade_out)
-                .replace(R.id.frame_container, fragment)
-                 .addToBackStack("myStack")
-                .commit();
-    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fragmentManager = getFragmentManager();
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         if (toolbar != null) {
@@ -51,20 +36,6 @@ public class MainActivity extends AppCompatActivity
                 .build();
 
 
-        Fragment fragment = new FragmentMain();
-        fragmentManager.beginTransaction().replace(R.id.frame_container, fragment)
-                .addToBackStack("myStack")
-                .commit();
-    }
-
-    @Override
-    public void onBackPressed()
-    {
-        sOnBackPressCounter++;
-        if(sOnBackPressCounter == 2){
-           super.onBackPressed();
-        }
-        fragmentManager.popBackStack();
 
     }
 }
